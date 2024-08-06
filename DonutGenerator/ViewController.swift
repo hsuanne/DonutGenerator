281420a49efb4b07d31e9af658163e7bc5fc9af7
Anne <hsuanne6@gmail.com>
2024-08-06 11:44:11 +0800

    add tableview

--- DonutGenerator/ViewController.swift
+++ DonutGenerator/ViewController.swift
@@ -8,34 +8,75 @@
 import UIKit
 import FoodTruckKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var doughImageView: UIImageView!
     @IBOutlet weak var glazeImageView: UIImageView!
     @IBOutlet weak var toppingImageView: UIImageView!
     @IBOutlet weak var donutSegmentControl: UISegmentedControl!
-    @IBOutlet weak var debugLabel: UILabel!
+    
+    @IBOutlet weak var tableview: UITableView!
+    var currentTableView: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        debugLabel.text = "000"
+        currentTableView = 0
     }
     
-    
-    @IBAction func onSegmentPressed(_ sender: Any) {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch donutSegmentControl.selectedSegmentIndex {
         case 0:
-            debugLabel.text = "0"
+            return Donut.Dough.all.count
             
         case 1:
-            debugLabel.text = "1"
-            
+            return Donut.Glaze.all.count
+
         case 2:
-            debugLabel.text = "2"
-            
+            return Donut.Topping.all.count
+
         default:
-            debugLabel.text = ""
+            return Donut.Dough.all.count
         }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "labelCell")
+        
+        
+        switch donutSegmentControl.selectedSegmentIndex {
+        case 0:
+            cell?.textLabel?.text = mapDoughToNames(doughs: Donut.Dough.all)[indexPath.row]
+            
+        case 1:
+            cell?.textLabel?.text = mapGlazeToNames(glazes: Donut.Glaze.all)[indexPath.row]
+
+        case 2:
+            cell?.textLabel?.text = mapToppingToNames(toppings: Donut.Topping.all)[indexPath.row]
+
+        default:
+            cell?.textLabel?.text = mapDoughToNames(doughs: Donut.Dough.all)[indexPath.row]
+        }
+        
+        
+        return cell!
+    }
+    
+    func mapDoughToNames(doughs: [Donut.Dough]) -> [String] {
+        return doughs.map {$0.name}
+    }
+    
+    func mapGlazeToNames(glazes: [Donut.Glaze]) -> [String] {
+        return glazes.map {$0.name}
+    }
+    
+    func mapToppingToNames(toppings: [Donut.Topping]) -> [String] {
+        return toppings.map {$0.name}
+    }
+        
+    @IBAction func onSegmentPressed(_ sender: Any) {
+        currentTableView = donutSegmentControl.selectedSegmentIndex
+        tableview.reloadData()
+
     }
     
     
